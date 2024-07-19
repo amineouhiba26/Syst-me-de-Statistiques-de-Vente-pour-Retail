99c52902cb14d90499f0122456fd4cec11424243
amineouhiba26 <amine.ouhiba@polytechnicien.tn>
2024-07-19 14:41:17 +0100

    ss

--- src/main/java/com/PSE/SSVR/prod.java
+++ src/main/java/com/PSE/SSVR/prod.java
@@ -1,2 +1,18 @@
-package com.PSE.SSVR;public class prod {
+package com.PSE.SSVR;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class prod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String prodName;
+    private String prodDescription;
+
 }
