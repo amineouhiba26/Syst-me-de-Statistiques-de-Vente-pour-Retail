0331e8c425cbbdb44f7fbb5e4af606a62612ee0e
amineouhiba26 <amine.ouhiba@polytechnicien.tn>
2024-07-19 14:21:08 +0100

    iting

---
+++ src/main/java/com/PSE/SSVR/SsvrApplication.java
@@ -0,0 +1,13 @@
+package com.PSE.SSVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsvrApplication.class, args);
+	}
+
+}
